bbe8b8923b3db424553b3e0df72a9cec7f9f28d1
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-08-12 14:03:50 +0300

    made the theme based on the system theme

--- lib/data/cubits/theme_cubit/cubit/theme_cubit.dart
+++ lib/data/cubits/theme_cubit/cubit/theme_cubit.dart
@@ -1,10 +1,19 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
 
 part 'theme_state.dart';
 
 class ThemeCubit extends Cubit<bool> {
-  ThemeCubit() : super(false);
+  ThemeCubit() : super(_getInitialThemeValue());
+
+  static bool _getInitialThemeValue() {
+    // Retrieve the current system brightness mode
+    final Brightness brightness =
+        WidgetsBinding.instance.window.platformBrightness;
+
+    // Return true for dark theme, false for light theme
+    return brightness == Brightness.dark;
+  }
 
   void toggleTheme() {
     emit(!state);
